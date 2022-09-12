4f5fd1e33820f4c25e1e967b82e69c7854136a8c
Vaibhav Singh <vaibhav.s@browserstack.com>
2022-09-12 21:07:31 +0530

    prepare for internal release

--- src/extension.ts
+++ src/extension.ts
@@ -10,12 +10,18 @@ const addSubscriptions = (context: vsCodeTypes.ExtensionContext): void => {
   const installNightwatch = (extension: NightwatchExt) => {
     extension.installNightwatch();
   };
+  const runAllTests = (extension: NightwatchExt) => extension.runAllTests();
 
   context.subscriptions.push(
     extensionManager.registerCommand({
       type: 'all-workspaces',
       name: 'install-nightwatch',
       callback: installNightwatch,
+    }),
+    extensionManager.registerCommand({
+      type: 'all-workspaces',
+      name: 'run-all-test',
+      callback: runAllTests,
     }),
     extensionManager.registerCommand({
       type: 'active-text-editor',
@@ -36,11 +42,8 @@ const addSubscriptions = (context: vsCodeTypes.ExtensionContext): void => {
       },
     }),
     vscode.workspace.onDidChangeConfiguration(extensionManager.onDidChangeConfiguration, extensionManager),
-
     vscode.workspace.onDidChangeWorkspaceFolders(extensionManager.onDidChangeWorkspaceFolders, extensionManager),
-
     vscode.workspace.onDidCloseTextDocument(extensionManager.onDidCloseTextDocument, extensionManager),
-
     vscode.window.onDidChangeActiveTextEditor(extensionManager.onDidChangeActiveTextEditor, extensionManager),
     vscode.workspace.onDidChangeTextDocument(extensionManager.onDidChangeTextDocument, extensionManager),
     vscode.workspace.onDidCreateFiles(extensionManager.onDidCreateFiles, extensionManager),
